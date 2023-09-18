08a2c4d7a8cd3e30169cb7f01426056e43eb9f10
ninalutz <nlutz@mit.edu>
2023-09-18 14:37:09 -0700

    see if it works on the internet

--- dragdropexport.js
+++ dragdropexport.js
@@ -1,52 +1,47 @@
+function allowDrop(ev) {
+  ev.preventDefault();
+}
 
+function dragStart(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
 
-      function allowDrop(ev) {
-          ev.preventDefault();
-      }
- 
-      function dragStart(ev) {
-          ev.dataTransfer.setData("text", ev.target.id);
-      }
- 
-      function dragDrop(ev) {
-          ev.preventDefault();
-          var data = ev.dataTransfer.getData("text");
-          ev.target.appendChild(document.getElementById(data));
-      }
+function dragDrop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
 
-      function exportData(){
-        console.log("HELLO")
-        let cells = document.getElementsByClassName('div1');
-        console.log(cells.length)
+function exportData(){
+let cells = document.getElementsByClassName('div1');
+console.log(cells.length)
 
-        let participant = document.getElementById("id").value;
+let participant = document.getElementById("id").value;
 
-        let row0 = ["box", "imageName"];
-        let rows =[];
-        rows.push(row0);
+let row0 = ["box", "imageName"];
+let rows =[];
+rows.push(row0);
 
-        for (var i = 0; i < cells.length; i++) {
-          let filename = cells[i].childNodes[1].src.split("/");
-          console.log(filename)
-          let row = [i+1, filename[filename.length-1]];
-          console.log(row)
-          rows.push(row);
-          }
+for (var i = 0; i < cells.length; i++) {
+  let filename = cells[i].childNodes[1].src.split("/");
+  console.log(filename)
+  let row = [i+1, filename[filename.length-1]];
+  console.log(row)
+  rows.push(row);
+  }
 
-        let csvContent = "data:text/csv;charset=utf-8,";
+let csvContent = "data:text/csv;charset=utf-8,";
 
-        rows.forEach(function(rowArray) {
-            let line = rowArray.join(",");
-            csvContent += line + "\r\n";
-        });
+rows.forEach(function(rowArray) {
+    let line = rowArray.join(",");
+    csvContent += line + "\r\n";
+});
 
-        var encodedUri = encodeURI(csvContent);
-        var link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", participant+".csv");
-        document.body.appendChild(link); // Required for FF
+var encodedUri = encodeURI(csvContent);
+var link = document.createElement("a");
+link.setAttribute("href", encodedUri);
+link.setAttribute("download", participant+".csv");
+document.body.appendChild(link); // Required for FF
 
-        link.click(); // This will download the data file named "my_data.csv".
-    }
-
-    
+link.click(); // This will download the data file named "my_data.csv".
+}   
