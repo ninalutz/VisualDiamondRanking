7fd8d8a05b7bc63d804573854b32f5ae262a79ce
ninalutz <nlutz@mit.edu>
2023-10-07 11:08:02 -0700

    add video that drags local

--- dragdropexport.js
+++ dragdropexport.js
@@ -57,6 +57,39 @@ exportData();
 
 }
 
+function exportVideoData(){
+    let cells = document.getElementsByClassName('pyramidbox');
+
+    let participant = document.getElementById("id").value;
+
+    let row0 = ["box", "videoName"];
+    let rows =[];
+    rows.push(row0);
+
+    for (var i = 0; i < cells.length; i++) {
+      for(var j = 1; j<cells[i].childNodes.length; j++){
+        console.log(cells[i].childNodes[j].id.split("drag")[1]);
+        let row = [i+1, cells[i].childNodes[j].id.split("drag")[1]];
+        rows.push(row);
+      }
+      }
+
+    let csvContent = "data:text/csv;charset=utf-8,";
+
+    rows.forEach(function(rowArray) {
+        let line = rowArray.join(",");
+        csvContent += line + "\r\n";
+    });
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", participant+".csv");
+    document.body.appendChild(link); // Required for FF
+
+    link.click(); // This will download the data file named "my_data.csv".
+}   
+
 function exportData(){
     let cells = document.getElementsByClassName('pyramidbox');
     console.log(cells.length)
