bce2f2790cb5ef0de75200dff0f39a654e678de1
ninalutz <ninalutz2015@gmail.com>
2023-10-10 19:39:13 -0700

    add 2 euro

--- slicedifusion.py
+++ slicedifusion.py
@@ -1,29 +1,37 @@
 import cv2
 
-directory = "GStudy/Gender/A woman/Africa/A Ghana woman/" #output directory
-filename = "../Ghana woman.png" #image to cut
 
-img = cv2.imread(filename)
-img2 = img
 
-height, width, channels = img.shape
+def process_images(NAME):
+      directory = "../Gender/A woman/Europe/" + NAME + "/" #output directory
+      filename = "../Gender/A woman/Europe/" + NAME +".png" #image to cut
 
-# Number of pieces Horizontally 
-W_SIZE  = 5 
-# Number of pieces Vertically to each Horizontal  
-H_SIZE = 10
+      img = cv2.imread(filename)
+      img2 = img
 
-counter = 0
+      height, width, channels = img.shape
 
-for ih in range(H_SIZE ):
-   for iw in range(W_SIZE ):
-   
-      x = width/W_SIZE * iw 
-      y = height/H_SIZE * ih
-      h = (height / H_SIZE)
-      w = (width / W_SIZE )
-      print(x,y,h,w)
-      counter+=1
-      img = img[int(y):int(y+h), int(x):int(x+w)]
-      cv2.imwrite(directory + str(counter) +  ".png",img)
-      img = img2
+      # Number of pieces Horizontally 
+      W_SIZE  = 5 
+      # Number of pieces Vertically to each Horizontal  
+      H_SIZE = 10
+
+      counter = 0
+
+      for ih in range(H_SIZE ):
+         for iw in range(W_SIZE ):
+         
+            x = width/W_SIZE * iw 
+            y = height/H_SIZE * ih
+            h = (height / H_SIZE)
+            w = (width / W_SIZE )
+            print(x,y,h,w)
+            counter+=1
+            img = img[int(y):int(y+h), int(x):int(x+w)]
+            cv2.imwrite(directory + str(counter) +  ".png",img)
+            img = img2
+
+names = ["A European woman", "A French woman", "A German woman", "A Russian woman", "An Italian woman", "UK"]
+
+for n in names:
+      process_images(n)
