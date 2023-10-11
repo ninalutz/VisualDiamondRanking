4f2714e0a05b3576151fe01f89657f941316cd06
ninalutz <ninalutz2015@gmail.com>
2023-10-10 20:03:30 -0700

    add euro p1

--- slicedifusion.py
+++ slicedifusion.py
@@ -3,8 +3,8 @@ import cv2
 
 
 def process_images(NAME):
-      directory = "../Gender/A person of nonbinary gender/Asia/" + NAME + "/" #output directory
-      filename = "../Gender/A person of nonbinary gender/Asia/" + NAME +".png" #image to cut
+      directory = "../Gender/A person of nonbinary gender/Europe/" + NAME + "/" #output directory
+      filename = "../Gender/A person of nonbinary gender/Europe/" + NAME +".png" #image to cut
 
       img = cv2.imread(filename)
       img2 = img
@@ -31,7 +31,7 @@ def process_images(NAME):
             cv2.imwrite(directory + str(counter) +  ".png",img)
             img = img2
 
-names = ["Asia nonbinary", "Bangladesh nonbinary", "China nonbinary", "India nonbinary", "Japan nonbinary", "Pakistan nonbinary"]
+names = ["Europe nonbinary", "France nonbinary", "German nonbinary", "Italy nonbinary", "Russia nonbinary", "UK nonbinary"]
 
 for n in names:
       process_images(n)
